8d04123ee5a06d743676c695a362d71b4a802df6
Denis Bain <baindenis95@gmail.com>
2024-02-15 10:43:30 -0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -1,16 +1,32 @@
-function something()
-{
-    var x = window.localStorage.getItem('bbb');
-    x = x*1 + 1;
-    window.localStorage.setItem('bbb', x);
-    alert(x);
+function something() {
+    alert('Hello');
 }
 
 function add_to_cart(id)
 {
     var key = 'product_' + id;
-    var x = window.localStorage.getItem(key)
-    x = x*1 + 1;
-    window.localStorage.setItem(key, x)
 
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
+
+    //Вывод количества item'ов в корзине
+    alert('Items in your cart: ' + get_number_of_items());
+}
+
+function get_number_of_items()
+{
+    var cnt = 0;
+
+    for(var i= 0 ; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); //получаем ключ
+        var value = window.localStorage.getItem(key); // пполучаем значение, аналог в руби hh[key] = x
+
+        if (key.indexOf('product_') == 0)
+        {
+            cnt = cnt + value*1;
+        }
+    }
+    return cnt;
 }
