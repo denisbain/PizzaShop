b4e900618905b4bd2470b940250c1389c9d2ec2d
Denis Bain <baindenis95@gmail.com>
2024-02-15 11:32:32 -0300

    Add cart_get_order

--- public/app.js
+++ public/app.js
@@ -30,3 +30,20 @@ function get_number_of_items()
     }
     return cnt;
 }
+
+function cart_get_orders()
+{
+    var orders = '';
+
+    for(var i= 0 ; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i); //получаем ключ
+        var value = window.localStorage.getItem(key); // пполучаем значение, аналог в руби hh[key] = x
+
+        if (key.indexOf('product_') == 0)
+        {
+            orders = orders + key + '=' + value + ',';
+        }
+    }
+    return orders;
+}
