65a284402ac2779b1217d29e9e3109f5503f71d5
Denis Bain <baindenis95@gmail.com>
2024-02-20 07:30:09 -0300

    Change type of item in the migration file

--- db/migrate/20240220101318_create_orders.rb
+++ db/migrate/20240220101318_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
     t.string :name
-    t.integer :phone
+    t.text :phone
     t.text :address
 
     t.timestamps
