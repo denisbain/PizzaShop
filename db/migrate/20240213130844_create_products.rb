3da5e07bc9e657fc05d02040f8e506d24e9bd502
Denis Bain <baindenis95@gmail.com>
2024-02-20 07:26:44 -0300

    Add model Order a

--- db/migrate/20240213130844_create_products.rb
+++ db/migrate/20240213130844_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[7.1]
   def change
     create_table :products do |t|
-      t. string :title
+      t.string :title
       t.text :description
       t.decimal :price
       t.decimal :size
---
+++ db/migrate/20240220101318_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :orders do |t|
+    t.string :name
+    t.integer :phone
+    t.text :address
+
+    t.timestamps
+    end
+  end
+end
