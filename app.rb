0c1acf8991db2a2e182a0ed020bf86c8a9508ddb
Denis Bain <baindenis95@gmail.com>
2024-02-22 12:27:33 -0300

    Display message if car is empty

--- app.rb
+++ app.rb
@@ -27,6 +27,10 @@ post '/cart' do
 	@orders_input = params[:orders_input]
 	@items = parse_orders_input @orders_input
 
+	if @items.length == 0
+		return erb :cart_is_empty
+	end
+
 	@items.each do |item|
 		product_id = item[0].to_i
 		product = Product.find(product_id)
